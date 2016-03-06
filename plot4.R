4a4d16e445aa85b75bd9e399a0f03d6076e9ea15
unknown <shankar29@gmail.com>
2016-03-06 16:04:57 -0600

    comitting the source and the output for plot4

---
+++ plot4.R
@@ -0,0 +1,40 @@
+## Download the file from the url and unzip it
+datSetFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(datSetFileUrl,"Dataset.zip")
+unzip("Dataset.zip")
+message("Dataset download and extraction - DONE.")
+
+message("Loading package: sqldf")
+library(sqldf)
+
+message("Loading package: datasets")
+library(datasets)
+
+message("Reading data from file . . .")
+dataFileName <- "household_power_consumption.txt"
+dataFile <- file(dataFileName)
+query <- "select * from dataFile where Date in ('1/2/2007','2/2/2007')"
+powerData <- sqldf(query, file.format= list(header=TRUE, sep=";"))
+close(dataFile)
+
+message("Generating png PLOT . . .")
+png(filename = "plot4.png",width = 480, height = 480, units = "px")
+par(mfrow=c(2,2))
+xaxisData <- strptime(paste(powerData$Date, powerData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+yaxisData <- powerData$Global_active_power
+plot(xaxisData, yaxisData, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+yaxisData <- powerData$Voltage
+plot(xaxisData, yaxisData, type="l", xlab="datetime", ylab="Voltage")
+
+plot(xaxisData, powerData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(xaxisData,powerData$Sub_metering_2, col="orange", type="l")
+lines(xaxisData,powerData$Sub_metering_3, col="blue", type="l")
+legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1,col=c("black", "orange","blue"))
+
+yaxisData <- powerData$Global_reactive_power
+plot(xaxisData, yaxisData, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+
+dev.off()
+message("DONE - Please refer to plot4.png.")
